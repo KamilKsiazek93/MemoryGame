f6abe04d4d47f5b955e7763d7eeadb9c9ff3266b
kamil.michal.ksiazek@gmail.com <kamil.michal.ksiazek@gmail.com>
2022-02-23 17:40:14 +0100

    Added method for getting words from file to list of string

---
+++ FilesOperations.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace MemoryGame
+{
+    public class FilesOperations
+    {
+        public List<string> GetWordsFromFile()
+        {
+            var words = new List<string>();
+            foreach (string line in File.ReadLines("../../../Words.txt"))
+            {
+                words.Add(line);
+            }
+            return words;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace MemoryGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var fileOperations = new FilesOperations();
+            var words = fileOperations.GetWordsFromFile();
+            foreach(string word in words)
+            {
+                Console.WriteLine(word);
+            }
+        }
+    }
+}
