8f2f4875a7ec5d6b45e0640d009ae4913c41106a
kamil.michal.ksiazek@gmail.com <kamil.michal.ksiazek@gmail.com>
2022-02-24 09:25:34 +0100

    Add method for prepare template for game

---
+++ Models/GameTemplate.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MemoryGame.Models
+{
+    public class GameTemplate
+    {
+        public string Key { get; set; }
+        public string Word { get; set; }
+        public Boolean IsDiscover { get; set; }
+    }
+}
